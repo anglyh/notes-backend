d83d8b37e4061bfb5ddadc9a3f8df40ffdfca29d
Angel Yaguno <angel.yaguno@tecsup.edu.pe>
2024-12-01 18:01:07 -0500

    front-end added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ let notes = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'));
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
